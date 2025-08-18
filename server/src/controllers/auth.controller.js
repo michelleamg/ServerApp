9104bebc1da31c834899951f243d363d4600f2b4
Alejandra Michelle Mateo Garcia <amateog1700@alumno.ipn.mx>
2025-08-18 08:21:47 -0600

    Refactor API to generic user model and endpoints
    Removed patient-specific routes, middleware, and diary features. Added generic user authentication and management controllers, routes, and configuration. Updated database connection to use new config. The API now supports user registration, login, profile updates, and deletion for a general user model instead of patient-only logic.

---
+++ server/src/controllers/auth.controller.js
@@ -0,0 +1,55 @@
+import { pool } from "../db.js";
+import bcrypt from "bcrypt";
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  bcrypt.hash(password, 10);
+  try {
+    const [rows] = await pool.query("SELECT * FROM Users WHERE email = ?", [
+      email,
+    ]);
+    console.log(rows);
+    if (rows.length > 0) {
+      const user = rows[0];
+      const match = await bcrypt.compare(password, user.user_password);
+      if (match) {
+        return res.status(200).json({
+          message: "Inicio de sesión exitoso",
+          user: rows,
+        });
+      }
+    }
+    res.status(401).json({ message: "Credenciales incorrectas" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error al iniciar sesión" });
+  }
+};
+
+export const register = async (req, res) => {
+  const { email } = req.body;
+  const [existingUser] = await pool.query(
+    "SELECT * FROM Users WHERE email = ?",
+    [email]
+  );
+  try {
+    const { username, email, password } = req.body;
+    if (!username || !email || !password) {
+      return res
+        .status(400)
+        .json({ message: "Todos los campos son obligatorios" });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const response = await pool.query(
+      "INSERT INTO Users (username, email, user_password) VALUES (?, ?, ?)",
+      [username, email, hashedPassword]
+    );
+    res.status(201).json({
+      message: "Usuario creado exitosamente",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.sqlMessage,
+    });
+  }
+};
