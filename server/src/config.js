9448e0725233fc903f8f8db73a7c758ba82a35c8
Alejandra Michelle Mateo Garcia <amateog1700@alumno.ipn.mx>
2025-08-18 21:24:17 -0600

    Refactor server structure and add user/auth endpoints
    Reorganized server code by introducing controllers and route files for authentication and user management. Updated configuration handling, removed unused patient.js, and implemented endpoints for login, registration, user updates, and deletion. Improved environment variable usage and simplified server startup.

--- server/src/config.js
+++ server/src/config.js
@@ -1,11 +1,11 @@
-export const config = {
-  PORT: process.env.PORT ?? 3000, 
-  DB_HOST: process.env.DB_HOST,
-  DB_PORT: process.env.DB_PORT,
-  DB_USER: process.env.DB_USER,
-  DB_PASS: process.env.DB_PASS,
-  DB_NAME: process.env.DB_NAME,
-  DB_CONN_LIMIT: process.env.DB_CONN_LIMIT,
-  JWT_SECRET: process.env.JWT_SECRET
-};
+import { config } from "dotenv";
 
+config();
+
+export const PORT = process.env.PORT || 3000;
+
+export const DB_HOST = process.env.DB_HOST || "20.84.60.252";
+export const DB_USER = process.env.DB_USER || "dev_user";
+export const DB_PASS = process.env.DB_PASS || "P4ssw0rd-S3gur0!";
+export const DB_DATABASE = process.env.DB_DATABASE || "TTAPOYOALDUELO";
+export const DB_PORT = process.env.DB_PORT || 3306;
