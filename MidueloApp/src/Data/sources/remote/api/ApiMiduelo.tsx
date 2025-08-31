5407669f9735fba3fb9c648e08ac6d3de84d92cf
Alejandra Michelle Mateo Garcia <amateog1700@alumno.ipn.mx>
2025-08-31 09:03:06 -0600

    Add safe-buffer and util-deprecate modules with TypeScript definitions
    - Introduced safe-buffer module with TypeScript definitions (index.d.ts) and implementation (index.js).
    - Updated package.json for safe-buffer with metadata and dependencies.
    - Added string_decoder module with safe-buffer as a dependency, including its own package.json.
    - Added util-deprecate module with browser support, including its implementation files (browser.js, node.js) and documentation (README.md).
    - Included history and license files for util-deprecate.

--- MidueloApp/src/Data/sources/remote/api/ApiMiduelo.tsx
+++ MidueloApp/src/Data/sources/remote/api/ApiMiduelo.tsx
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const API_Miduelo = axios.create({
-  baseURL: "https://192.168.1.72:3000/api",
+  baseURL: "https://192.168.1.72:3000/login",
   headers: {
     "Content-Type": "application/json",
-    
+
   },
 });
 
-export default API_Miduelo;
+export {API_Miduelo};
--- MidueloApp/src/Presentation/view/home/ViewModel.tsx
+++ MidueloApp/src/Presentation/view/home/ViewModel.tsx
@@ -1,4 +1,5 @@
 import React, {useState} from "react";
+import { API_Miduelo } from "../../../Data/sources/remote/api/ApiMiduelo";
 
 const HomeViewModel = () => {
     const [values, setValues] = useState(
@@ -9,10 +10,19 @@ const HomeViewModel = () => {
     const onChange = (property: string, value: any) => {
         setValues({...values, [property]: value});
     }
+    const login = async () => {
+        try {
+            const response = await API_Miduelo.post("/login", values);
+            console.log("Login successful:", JSON.stringify(response.data));
+        } catch (error) {
+            console.error("Login failed:", error);
+        }
+    }
     return {
         ...values,
         onChange
     }
 }
+
 
 export default HomeViewModel;
