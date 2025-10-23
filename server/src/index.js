18456fc5c8d4f54aed83e83551aeb3ff637e43ca
Amsmonroe <amerik020208@gmail.com>
2025-10-23 15:03:03 -0600

    correcciones

--- server/src/index.js
+++ server/src/index.js
@@ -1,18 +1,18 @@
+// src/index.js
 import dotenv from "dotenv";
 import path from "path";
 import { fileURLToPath } from "url";
 import { PORT } from "./db/config.js";
+import app from "./app.js";
 
+// 🧭 Definir __dirname correctamente
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // ✅ Cargar el .env que está en ~/ServerApp/.env
 dotenv.config({ path: path.resolve(__dirname, "../../.env") });
 
-import app from "./app.js";
-import { PORT } from "./db/config.js";
-
+// 🚀 Iniciar servidor
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Server running on http://0.0.0.0:${PORT}`);
 });
-
