d98c33b60d1b4b50b2fa09af7c60c0bfbc546b58
Amsmonroe <amerik020208@gmail.com>
2025-10-23 14:58:43 -0600

    ajustes de index.js

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import path from "path";
 import { fileURLToPath } from "url";
+import { PORT } from "./db/config.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
