115e8bd68e9b2cf93aef28cb0482bc68fcec5cc3
michelleamg <145891150+michelleamg@users.noreply.github.com>
2025-10-30 06:45:11 -0600

    Rutas del perfil

--- server/src/app.js
+++ server/src/app.js
@@ -9,6 +9,7 @@ import authRoutes from "./routes/auth.routes.js";
 import consentimientoRoutes from "./routes/consentimiento.routes.js";
 import testRoutes from "./routes/test.routes.js"
 import agendaRoutes from "./routes/agenda.routes.js";
+import pacientesRoutes from "./routes/pacientes.routes.js";
 
 const app = express();
 
@@ -31,6 +32,9 @@ app.use("/api", indexRoutes);
 app.use("/api/consentimientos", consentimientoRoutes);
 app.use("/api/agenda", agendaRoutes);
 app.use("/api/tests", testRoutes);
+app.use("/api/pacientes",pacientesRoutes);
+
+
 // Ruta de prueba
 app.get("/api/ping", (req, res) => {
   res.json({ message: "pong", timestamp: new Date() });
