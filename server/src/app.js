02a856c9c61b5c2958cb317c6a6d1416d5894d8d
Amsmonroe <amerik020208@gmail.com>
2025-11-07 02:00:57 -0600

    renombre la ruta base a plural de foro a foros

--- server/src/app.js
+++ server/src/app.js
@@ -40,7 +40,7 @@ app.use("/api/pacientes",pacientesRoutes);
 app.use("/api/diario-emociones", diarioEmocionesRoutes);
 app.use("/api/actividades", ActividadesRoutes);
 app.use("/api", chatRoutes);
-app.use("/api/foro", foroRoutes);
+app.use("/api/foros", foroRoutes);
 // Ruta de prueba
 app.get("/api/ping", (req, res) => {
   res.json({ message: "pong", timestamp: new Date() });
