7dafa555975c0a06d4eb95c77deea498437b37aa
michelleamg <145891150+michelleamg@users.noreply.github.com>
2025-11-11 12:53:13 -0600

    Agregue id poacienrte

--- server/src/routes/recursos.routes.js
+++ server/src/routes/recursos.routes.js
@@ -4,6 +4,6 @@ import { RecursosController } from "../controllers/recursosController.js";
 const router = express.Router();
 
 // GET /api/recursos
-router.get("/recursos", RecursosController.getRecursos);
+router.get("/recursos/:id_paciente", RecursosController.getRecursos);
 
 export default router;
